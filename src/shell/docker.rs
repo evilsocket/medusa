019d474b97b09790b8bb36d08e3e0a9dce6a155c
Simone Margaritelli <evilsocket@gmail.com>
2021-09-03 01:02:22 +0200

    new: docker commands are now executed via HTTP requests and don't require the docker binary anymore

--- src/shell/docker.rs
+++ src/shell/docker.rs
@@ -1,4 +1,10 @@
+use std::collections::HashMap;
+use std::io::prelude::*;
+use std::os::unix::net::UnixStream;
+
 use log::debug;
+
+const DOCKER_SOCKET: &str = "/var/run/docker.sock";
 
 pub fn exec(container_id: &str, command: &str) -> Result<String, String> {
 	debug!(
@@ -6,28 +12,66 @@ pub fn exec(container_id: &str, command: &str) -> Result<String, String> {
 		command, container_id
 	);
 
-	let args = vec!["exec", container_id, "sh", "-c", command];
+	// create exec operation
+	let mut stream = UnixStream::connect(DOCKER_SOCKET)
+		.map_err(|e| format!("could not connect to {}: {}", DOCKER_SOCKET, e))?;
 
-	debug!("docker {:?}", args);
-	// TODO: replace with https://github.com/softprops/shiplift
-	let output = std::process::Command::new("docker")
-		.args(&args)
-		.output()
-		.unwrap();
-
-	let mut data = String::from_utf8_lossy(&output.stdout).to_string();
-
-	data += String::from_utf8_lossy(&output.stderr).trim();
-	data = data.replace("\n", "\r\n");
-
-	debug!(
-		"{}",
-		if data.len() <= 100 {
-			&data
-		} else {
-			&data[..100]
-		}
+	let content = format!(
+		"{{
+		\"AttachStdout\": true,
+		\"Tty\": true,
+		\"Cmd\": [ \"sh\", \"-c\", {}]
+	  }}",
+		serde_json::to_string(command).unwrap()
 	);
+	let request = format!("POST /containers/{}/exec HTTP/1.0\r\n", container_id)
+		+ "Content-Type: application/json\r\n"
+		+ &format!("Content-Length: {}\r\n", content.len())
+		+ &format!("\r\n{}", content);
 
-	Ok(data)
+	debug!("{}\n\n", &request);
+	stream
+		.write_all(request.as_bytes())
+		.map_err(|e| format!("could not write to {}: {}", DOCKER_SOCKET, e))?;
+
+	let mut response = String::new();
+	stream
+		.read_to_string(&mut response)
+		.map_err(|e| format!("could not read from {}: {}", DOCKER_SOCKET, e))?;
+
+	let response = format!("{{{}", response.splitn(2, '{').nth(1).unwrap());
+	let response: HashMap<String, String> = serde_json::from_str(&response).unwrap();
+	let exec_id = response.get("Id").unwrap();
+	debug!("exec_id = '{}'", exec_id);
+
+	drop(stream);
+
+	// start exec operation by id
+	let mut stream = UnixStream::connect(DOCKER_SOCKET)
+		.map_err(|e| format!("could not connect to {}: {}", DOCKER_SOCKET, e))?;
+
+	let content = "{
+			\"Detach\": false,
+			\"Tty\": true
+		  }";
+	let request = format!("POST /exec/{}/start HTTP/1.0\r\n", exec_id)
+		+ "Content-Type: application/json\r\n"
+		+ &format!("Content-Length: {}\r\n", content.len())
+		+ &format!("\r\n{}", content);
+
+	debug!("{}\n\n", &request);
+	stream
+		.write_all(request.as_bytes())
+		.map_err(|e| format!("could not write to {}: {}", DOCKER_SOCKET, e))?;
+
+	let mut response = String::new();
+	stream
+		.read_to_string(&mut response)
+		.map_err(|e| format!("could not read from {}: {}", DOCKER_SOCKET, e))?;
+
+	debug!("{}", &response);
+
+	let response = response.splitn(2, "\r\n\r\n").nth(1).unwrap();
+
+	Ok(response.trim().to_owned())
 }
